6b3b3b75e2a97bcefdc436a8dd5331c0c50abd3e
marcoturetta <marco.turetta@blinklastmile.com>
2023-03-24 06:57:29 +0100

    feat: exp

--- test2.sh
+++ test2.sh
@@ -1,2 +1,2 @@
-python3 comparison_seeded.py -f car_evaluation -d 3 -t 3600
-python3 comparison_seeded.py -f hayes-roth -d 3 -t 3600
+python3 comparison_seeded.py -f car_evaluation -d 4 -t 3600
+python3 comparison_seeded.py -f hayes-roth -d 4 -t 3600
