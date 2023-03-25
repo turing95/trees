1ecb5abd6cb73535441b6d01f44327e262c8efe8
marcoturetta <marco.turetta@blinklastmile.com>
2023-03-25 07:41:53 +0100

    feat: exp

--- test.sh
+++ test.sh
@@ -1,16 +1,3 @@
 python3 FlowORT_light_continuous_linear_multivariate_replication.py -f yacht_hydrodynamics_reg.csv -d 4 -t 0 -i 1
 python3 FlowORT_light_continuous_linear_multivariate_replication.py -f yacht_hydrodynamics_reg.csv -d 4 -t 360 -i 1
-python3 FlowORT_light_continuous_linear_multivariate_replication.py -f yacht_hydrodynamics_reg.csv -d 4 -t 360 -i 0
-python3 FlowORT_light_continuous_linear_multivariate_replication.py -f abalone_reg.csv -d 4 -t 0 -i 1
-python3 FlowORT_light_continuous_linear_multivariate_replication.py -f abalone_reg.csv -d 4 -t 360 -i 1
-python3 FlowORT_light_continuous_linear_multivariate_replication.py -f abalone_reg.csv -d 4 -t 360 -i 0
-python3 FlowORT_light_continuous_linear_multivariate_replication.py -f auto-mpg_reg.csv -d 4 -t 0 -i 1
-python3 FlowORT_light_continuous_linear_multivariate_replication.py -f auto-mpg_reg.csv -d 4 -t 360 -i 1
-python3 FlowORT_light_continuous_linear_multivariate_replication.py -f auto-mpg_reg.csv -d 4 -t 360 -i 0
-python3 FlowORT_light_continuous_linear_multivariate_replication.py -f winequality-white_reg.csv -d 4 -t 0 -i 1
-python3 FlowORT_light_continuous_linear_multivariate_replication.py -f winequality-white_reg.csv -d 4 -t 360 -i 1
-python3 FlowORT_light_continuous_linear_multivariate_replication.py -f winequality-white_reg.csv -d 4 -t 360 -i 0
-python3 FlowORT_light_continuous_linear_multivariate_replication.py -f airfoil_self_noise_reg.csv -d 4 -t 0 -i 1
-python3 FlowORT_light_continuous_linear_multivariate_replication.py -f airfoil_self_noise_reg.csv -d 4 -t 360 -i 1
-python3 FlowORT_light_continuous_linear_multivariate_replication.py -f airfoil_self_noise_reg.csv -d 4 -t 360 -i 0
 
