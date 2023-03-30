0468cf025f02ec865bdfe1f6faed75d4e15ed07d
turing95 <marco1491995@gmail.com>
2023-03-30 10:24:52 +0000

    test

--- test_aut.sh
+++ test_aut.sh
@@ -1,11 +1,3 @@
-python3 FlowORT_light_continuous_linear_multivariate_replication.py -f auto-mpg_reg.csv -d 2 -t 0 -i 1
-python3 FlowORT_light_continuous_linear_multivariate_replication.py -f auto-mpg_reg.csv -d 2 -t 3600 -i 1
-python3 FlowORT_light_continuous_linear_multivariate_replication.py -f auto-mpg_reg.csv -d 2 -t 3600 -i 0
-python3 FlowORT_light_continuous_linear_multivariate_replication.py -f auto-mpg_reg.csv -d 3 -t 0 -i 1
-python3 FlowORT_light_continuous_linear_multivariate_replication.py -f auto-mpg_reg.csv -d 3 -t 3600 -i 1
-python3 FlowORT_light_continuous_linear_multivariate_replication.py -f auto-mpg_reg.csv -d 3 -t 3600 -i 0
-python3 FlowORT_light_continuous_linear_multivariate_replication.py -f auto-mpg_reg.csv -d 4 -t 0 -i 1
-python3 FlowORT_light_continuous_linear_multivariate_replication.py -f auto-mpg_reg.csv -d 4 -t 3600 -i 1
-python3 FlowORT_light_continuous_linear_multivariate_replication.py -f auto-mpg_reg.csv -d 4 -t 3600 -i 
+python3 FlowORT_light_continuous_linear_multivariate_replication.py -f auto-mpg_reg.csv -d 4 -t 3600 -i 0 
 
 
