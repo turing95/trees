9038ba2b9fd182ff4b542f7a2acec416dfab6884
marcoturetta <marco.turetta@blinklastmile.com>
2023-04-02 17:03:25 +0200

    feat: res

--- test_air.sh
+++ test_air.sh
@@ -1,3 +1,3 @@
-python3 FlowORT_light_continuous_linear_multivariate_replication.py -f airfoil_self_noise_reg.csv -d 1 -t 1 -i 1
+python3 FlowORT_light_continuous_linear_multivariate_replication.py -f airfoil_self_noise_reg.csv -d 5 -t 1 -i 1
 python3 FlowORT_light_continuous_linear_multivariate_replication.py -f airfoil_self_noise_reg.csv -d 5 -t 3600 -i 1
 python3 FlowORT_light_continuous_linear_multivariate_replication.py -f airfoil_self_noise_reg.csv -d 5 -t 3600 -i 0
