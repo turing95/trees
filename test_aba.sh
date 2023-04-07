b1bc686427794a010f540e207aaf3e387ccc7556
marcoturetta <marco.turetta@blinklastmile.com>
2023-04-07 13:18:48 +0200

    feat: res

--- test_aba.sh
+++ test_aba.sh
@@ -1,6 +1,6 @@
-python3 FlowORT_light_continuous_linear_multivariate_replication.py -f abalone_reg.csv -d 3 -t 1 -i 1
-python3 FlowORT_light_continuous_linear_multivariate_replication.py -f abalone_reg.csv -d 3 -t 3600 -i 1
-python3 FlowORT_light_continuous_linear_multivariate_replication.py -f abalone_reg.csv -d 3 -t 3600 -i 0
+python3 FlowORT_light_continuous_linear_multivariate_replication.py -f abalone_reg.csv -d 4 -t 1 -i 1
+python3 FlowORT_light_continuous_linear_multivariate_replication.py -f abalone_reg.csv -d 4 -t 3600 -i 1
+python3 FlowORT_light_continuous_linear_multivariate_replication.py -f abalone_reg.csv -d 4 -t 3600 -i 0
 
 
 
